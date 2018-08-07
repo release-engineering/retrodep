a00da94aae03521a14109f5033efc1fb120dc376
Tim Waugh <twaugh@redhat.com>
2018-08-07 11:41:30 +0100

    Initial implementation of WorkingTree and GoSource
    Signed-off-by: Tim Waugh <twaugh@redhat.com>

---
+++ gosource.go
@@ -0,0 +1,150 @@
+// Copyright (C) 2018 Tim Waugh
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package backvendor
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	"golang.org/x/tools/go/vcs"
+)
+
+func pathStartsWith(dir, prefix string) bool {
+	plen := len(prefix)
+	if len(dir) <= plen {
+		return dir == prefix
+	}
+
+	return dir[:plen] == prefix && dir[plen] == filepath.Separator
+}
+
+type vendoredSearch struct {
+	// Path to the "vendor" directory
+	vendor string
+
+	// Path to last project identified
+	lastdir string
+
+	// Vendored packages, indexed by Root
+	vendored map[string]*vcs.RepoRoot
+}
+
+func (s *vendoredSearch) inLastDir(pth string) bool {
+	return s.lastdir != "" && pathStartsWith(pth, s.lastdir)
+}
+
+func processVendoredSource(search *vendoredSearch, pth string) error {
+	// For .go source files, see which directory they are in
+	thisimport := filepath.Dir(pth[1+len(search.vendor):])
+	reporoot, err := vcs.RepoRootForImportPath(thisimport, false)
+	if err != nil {
+		return err
+	}
+
+	// The project name is relative to the vendor dir
+	search.vendored[reporoot.Root] = reporoot
+	search.lastdir = filepath.Join(search.vendor, reporoot.Root)
+	return nil
+}
+
+// GoSource represents a filesystem tree containing Go source code.
+type GoSource string
+
+// Topdir returns the top-level path of the filesystem tree.
+func (src GoSource) Topdir() string {
+	return string(src)
+}
+
+// Vendor returns the path to the vendored source code.
+func (src GoSource) Vendor() string {
+	return filepath.Join(src.Topdir(), "vendor")
+}
+
+// VendoreredProjects return a map of project import names to information
+// about those projects, including which version control system they use.
+func (src GoSource) VendoredProjects() (map[string]*vcs.RepoRoot, error) {
+	search := vendoredSearch{
+		vendor:   src.Vendor(),
+		vendored: make(map[string]*vcs.RepoRoot, 0),
+	}
+	walkfn := func(pth string, info os.FileInfo, err error) error {
+		if err != nil {
+			// Stop on error
+			return err
+		}
+
+		// Ignore paths within the last project we identified
+		if search.inLastDir(pth) {
+			return nil
+		}
+
+		// Ignore anything except Go source
+		if !info.Mode().IsRegular() || !strings.HasSuffix(pth, ".go") {
+			return nil
+		}
+
+		// Identify the project
+		return processVendoredSource(&search, pth)
+	}
+	if _, err := os.Stat(search.vendor); err != nil {
+		return nil, err
+	}
+
+	err := filepath.Walk(search.vendor, walkfn)
+	if err != nil {
+		return nil, err
+	}
+
+	return search.vendored, nil
+}
+
+// DescribeVendoredProject attempts to identify the tag in the version
+// control system which corresponds to the vendored copy of the
+// project.
+func (src GoSource) DescribeVendoredProject(project *vcs.RepoRoot) (string, error) {
+	pwt, err := NewWorkingTree(project)
+	if err != nil {
+		return "", err
+	}
+	defer pwt.Close()
+
+	projectdir := filepath.Join(src.Vendor(), project.Root)
+	hashes, err := NewFileHashes(pwt.VCS.Cmd, projectdir)
+	if err != nil {
+		return "", err
+	}
+	tags, err := pwt.SemVerTags()
+	if err != nil {
+		return "", err
+	}
+	matched := ""
+	for _, tag := range tags {
+		match, err := pwt.FileHashesAreSubset(hashes, tag)
+		if err != nil {
+			return "", err
+		}
+		if match {
+			matched = tag
+			break
+		}
+	}
+	if matched == "" {
+		return "", VersionNotFound
+	}
+
+	return matched, nil
+}
---
+++ workingtree.go
@@ -0,0 +1,136 @@
+// Copyright (C) 2018 Tim Waugh
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package backvendor
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"sort"
+	"strings"
+
+	"github.com/Masterminds/semver"
+	"golang.org/x/tools/go/vcs"
+)
+
+// A WorkingTree is a local checkout of Go source code, and
+// information about the version control system it came from.
+type WorkingTree struct {
+	Source GoSource
+	VCS    *vcs.Cmd
+}
+
+// NewWorkingTree creates a local checkout of the version control
+// system for a Go project.
+func NewWorkingTree(project *vcs.RepoRoot) (*WorkingTree, error) {
+	dir, err := ioutil.TempDir("", "go-backvendor.")
+	err = project.VCS.Create(dir, project.Repo)
+	if err != nil {
+		return nil, err
+	}
+
+	return &WorkingTree{
+		Source: GoSource(dir),
+		VCS:    project.VCS,
+	}, nil
+}
+
+// Close removes the local checkout.
+func (wt *WorkingTree) Close() error {
+	return os.RemoveAll(wt.Source.Topdir())
+}
+
+// SemVerTags returns a list of the semantic tags, i.e. those tags which are
+// parseable as semantic tags such as v1.1.0.
+func (wt *WorkingTree) SemVerTags() ([]string, error) {
+	tags, err := wt.VCS.Tags(wt.Source.Topdir())
+	if err != nil {
+		return nil, err
+	}
+	semvers := make(semver.Collection, 0)
+	semvertags := make(map[*semver.Version]string, 0)
+	for _, tag := range tags {
+		v, err := semver.NewVersion(tag)
+		if err != nil {
+			continue
+		}
+		semvers = append(semvers, v)
+		semvertags[v] = tag
+	}
+	sort.Sort(sort.Reverse(semvers))
+	strtags := make([]string, len(semvers))
+	for i, v := range semvers {
+		strtags[i] = semvertags[v]
+	}
+	return strtags, nil
+}
+
+// FileHashesAreSubset compares a set of files and their hashes with
+// those from a particular tag. It returns true if the provided files
+// and hashes are a subset of those found at the tag.
+func (wt *WorkingTree) FileHashesAreSubset(fh FileHashes, tag string) (bool, error) {
+	if wt.VCS.Cmd != "git" {
+		return false, fmt.Errorf("FileHashesAreSubset not implemented for %s",
+			wt.VCS.Cmd)
+	}
+
+	args := []string{"ls-tree", "-r", tag}
+	cmd := exec.Command(wt.VCS.Cmd, args...)
+	var buf bytes.Buffer
+	cmd.Stdout = &buf
+	cmd.Stderr = &buf
+	cmd.Dir = wt.Source.Topdir()
+	err := cmd.Run()
+	if err != nil {
+		if strings.HasPrefix(buf.String(), "fatal: Not a valid object name ") {
+			// This is a branch name, not a tag name
+			return false, nil
+		}
+
+		os.Stderr.Write(buf.Bytes())
+		return false, err
+	}
+	tagfilehashes := make(FileHashes)
+	scanner := bufio.NewScanner(&buf)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Fields(line)
+		if len(fields) < 4 {
+			return false, fmt.Errorf("line not understood: %s", line)
+		}
+
+		var mode uint32
+		if _, err = fmt.Sscanf(fields[0], "%o", &mode); err != nil {
+			return false, err
+		}
+		tagfilehashes[fields[3]] = FileHash(fields[2])
+	}
+	for path, filehash := range fh {
+		tagfilehash, ok := tagfilehashes[path]
+		if !ok {
+			// File not present in tag
+			return false, nil
+		}
+		if filehash != tagfilehash {
+			// Hash does not match
+			return false, nil
+		}
+	}
+	return true, nil
+}
